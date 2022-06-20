412812e9538d4daaa06be7890ef97b0cc96ec258
henrynchallo <107813268+henrynchallo@users.noreply.github.com>
2022-06-20 15:08:08 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,13 +7,14 @@ $dv1=$_POST['dv1'];
 $dv2=$_POST['dv2'];
 $occ=$_POST['occ'];
 $phonenumber=$_POST['phonenumber'];
-$email=$_POST['email']; $conn=new mysqli('localhost','root','','alumni');
+$email=$_POST['email']; 
+$conn=new mysqli('localhost','root','','alumni');
 if($conn->connect_error){
     die('connection failed :'.$conn->connect_error);
 }else{
     $stmt = $conn->prepare("insert into registration(name, yoe, yog, fteacher, dv1, dv2, occ, phonenumber, email)"
                           values(?, ?, ?, ?, ?, ?, ?, ?, ?));
-    $stmt->bind_param("sssssssis", $name, $yoe, $yog, $fteacher, $dv1, $dv2, $occ, $phonenumber, $email);
+    $stmt->bind_param( $name, $yoe, $yog, $fteacher, $dv1, $dv2, $occ, $phonenumber, $email);
     $stmt->execute();
     echo "registration successfully...";
     $stmt->close();
